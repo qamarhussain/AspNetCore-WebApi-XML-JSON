94ae9f3a7f970a76e6b8eafcda39f734d08927c2
Qamar Hussain <qamar.hussain@aspirastech.com>
2020-05-01 23:46:03 +0500

    first commit

---
+++ AspNetWebApiXMLJson/Controllers/EmployeeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AspNetWebApiXMLJson.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AspNetWebApiXMLJson.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        [HttpGet("get.{format}"),FormatFilter]
+        public IEnumerable<Employee> Get()
+        {
+            var employees = new List<Employee>
+            {
+                new Employee{Id=1,FirstName="Qamar",LastName="Hussain"},
+                new Employee{Id=1,FirstName="Ali",LastName="Abid"}
+            };
+            return employees;
+        }
+
+        [HttpPost("post.{format}"), FormatFilter]
+        public Employee Post([FromBody]Employee employee)
+        {
+            if (ModelState.IsValid)
+            {
+                return employee;
+            }
+            return null;
+        }
+    }
+}
