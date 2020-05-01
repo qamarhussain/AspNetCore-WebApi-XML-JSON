a2fa8fed2ab3124a4952c3789c1ffe0b14d2789b
Qamar Hussain <qamar.hussain@aspirastech.com>
2020-05-02 00:06:24 +0500

    screen samples added.

--- AspNetWebApiXMLJson/Controllers/EmployeeController.cs
+++ AspNetWebApiXMLJson/Controllers/EmployeeController.cs
@@ -1,9 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 using AspNetWebApiXMLJson.Models;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace AspNetWebApiXMLJson.Controllers
--- AspNetWebApiXMLJson/Models/Employee.cs
+++ AspNetWebApiXMLJson/Models/Employee.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace AspNetWebApiXMLJson.Models
+﻿namespace AspNetWebApiXMLJson.Models
 {
     public class Employee
     {
